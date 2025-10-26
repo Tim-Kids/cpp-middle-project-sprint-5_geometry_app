bbbd8cf51ff21aba1b7522ebf93ba3e3172a41cd
timkids <timkids@czr.org>
2025-10-26 18:06:27 +0300

    Initial commit.

---
+++ cmake-build-debug/CMakeFiles/fc-tmp/cpm/download.cmake
@@ -0,0 +1,9 @@
+cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
+
+message(VERBOSE "Executing download step for cpm")
+
+block(SCOPE_FOR VARIABLES)
+
+include("/tmp/cpp-middle-project-sprint-5-main/cmake-build-debug/CMakeFiles/fc-tmp/cpm/cpm-gitclone.cmake")
+
+endblock()
