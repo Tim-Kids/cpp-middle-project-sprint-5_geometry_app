c9cdc891016d03dec284acb4c482c475b4c9463b
timkids <timkids@czr.org>
2025-10-26 18:15:08 +0300

    REF: рефакторинг дерева каталогов проекта.

--- cmake-build-debug/CMakeFiles/fc-tmp/cpm/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing update step for cpm")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/tmp/cpp-middle-project-sprint-5-main/cmake-build-debug/CMakeFiles/fc-tmp/cpm/cpm-gitupdate.cmake")
-
-endblock()
